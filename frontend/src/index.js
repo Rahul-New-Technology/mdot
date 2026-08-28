88c992c7ec12b2f95164725f6fb386faba3dd57e
Aamir Khan <aamirkhan91613216@gmail.com>
2026-08-28 05:47:43 -0700

    Fix merge conflicts in index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-<<<<<<< HEAD
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-=======
->>>>>>> fba28f1927745d4e7ea5a9f5be5d999464fa178d
 import { HelmetProvider } from "react-helmet-async";
 import "@/index.css";
 import App from "@/App";
 
-<<<<<<< HEAD
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -18,19 +14,13 @@ const queryClient = new QueryClient({
   },
 });
 
-=======
->>>>>>> fba28f1927745d4e7ea5a9f5be5d999464fa178d
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <HelmetProvider>
-<<<<<<< HEAD
       <QueryClientProvider client={queryClient}>
         <App />
       </QueryClientProvider>
-=======
-      <App />
->>>>>>> fba28f1927745d4e7ea5a9f5be5d999464fa178d
     </HelmetProvider>
   </React.StrictMode>,
 );
